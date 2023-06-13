27f9276c4dab5fdd8b8def8f32b7c0b31e6d4bd2
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-06-13 17:57:25 +0300

    Starting new project

---
+++ Controllers/CitiesInfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
