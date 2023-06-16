4347252964ae6c863272caa29a50b09f61250d3d
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-06-16 21:19:18 +0300

    point of interest controller

---
+++ Controllers/PointOfInterestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class PointOfInterestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
